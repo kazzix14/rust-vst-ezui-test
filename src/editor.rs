e8e28c74dbecaeb60f704d168f544106634fb129
kazuma murata <atugari27@gmail.com>
2019-09-02 01:30:26 +0900

    add editor.rs
    move "struct MyEditor" and related things

---
+++ src/editor.rs
@@ -0,0 +1,60 @@
+use vst::editor::Editor;
+use derive_builder::Builder;
+
+#[derive(Builder)]
+struct MyEditor {
+    #[builder(default = "(200, 200)")]
+    size: (i32, i32),
+
+    #[builder(default = "(0, 0)")]
+    position: (i32, i32),
+
+    #[builder(default = "false", setter(skip))]
+    is_open: bool,
+
+    params: Arc<MyParameters>,
+
+    #[builder(default = "None", setter(skip))]
+    ui: Option<UiState>,
+}
+
+impl Editor for MyEditor {
+    fn size(&self) -> (i32, i32) {
+        self.size
+    }
+
+    fn position(&self) -> (i32, i32) {
+        self.position
+    }
+
+    fn idle(&mut self) {
+        if let Some(ui) = &mut self.ui {
+            ui.draw(Arc::clone(&self.params));
+        }
+    }
+
+    fn close(&mut self) {
+        self.ui = None;
+    }
+
+    fn open(&mut self, parent: *mut c_void) -> bool {
+        let wb = winit::WindowBuilder::new()
+            .with_dimensions(self.size.0 as u32, self.size.1 as u32)
+            .with_decorations(false)
+            .with_parent(parent);
+
+        match conrod::glium::glutin::WindowBuilder::from_winit_builder(wb)
+            .with_multisampling(4)
+            .build_glium()
+        {
+            Ok(display) => self.ui = Some(UiState::new(Path::new("."), display)),
+            Err(_) => (),
+        }
+
+        self.is_open()
+    }
+
+    fn is_open(&mut self) -> bool {
+        self.ui.is_some()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+mod editor;
+use editor::MyEditor;
+use editor::MyEditorBuilder;
+
 use conrod::glium;
 use conrod::glium::DisplayBuild;
 use conrod::glium::Surface;
@@ -53,23 +57,6 @@ struct MyPlugin {
     note: Option<u8>,
     params: Arc<MyParameters>,
     editor_returned: bool,
-}
-
-#[derive(Builder)]
-struct MyEditor {
-    #[builder(default = "(200, 200)")]
-    size: (i32, i32),
-
-    #[builder(default = "(0, 0)")]
-    position: (i32, i32),
-
-    #[builder(default = "false", setter(skip))]
-    is_open: bool,
-
-    params: Arc<MyParameters>,
-
-    #[builder(default = "None", setter(skip))]
-    ui: Option<UiState>,
 }
 
 impl UiState {
@@ -179,46 +166,6 @@ fn set_widgets(params: Arc<MyParameters>, ref mut ui: conrod::UiCell, ids: &mut 
     */
 }
 
-impl Editor for MyEditor {
-    fn size(&self) -> (i32, i32) {
-        self.size
-    }
-
-    fn position(&self) -> (i32, i32) {
-        self.position
-    }
-
-    fn idle(&mut self) {
-        if let Some(ui) = &mut self.ui {
-            ui.draw(Arc::clone(&self.params));
-        }
-    }
-
-    fn close(&mut self) {
-        self.ui = None;
-    }
-
-    fn open(&mut self, parent: *mut c_void) -> bool {
-        let wb = winit::WindowBuilder::new()
-            .with_dimensions(self.size.0 as u32, self.size.1 as u32)
-            .with_decorations(false)
-            .with_parent(parent);
-
-        match conrod::glium::glutin::WindowBuilder::from_winit_builder(wb)
-            .with_multisampling(4)
-            .build_glium()
-        {
-            Ok(display) => self.ui = Some(UiState::new(Path::new("."), display)),
-            Err(_) => (),
-        }
-
-        self.is_open()
-    }
-
-    fn is_open(&mut self) -> bool {
-        self.ui.is_some()
-    }
-}
 
 struct MyParameters {
     attack: AtomicFloat,
